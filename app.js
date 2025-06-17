9ba9f07787137aee574e20c655e02e1dd7cbdd12
MarcosAJLC <marcosalexandrejlc@gmail.com>
2025-06-17 00:00:53 -0300

    testando app

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ this.app.use((req, res, next) => {
 
 const whiteList = ["http://localhost:3000"];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = express();
@@ -31,6 +41,7 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
     this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());

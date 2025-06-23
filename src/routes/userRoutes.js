6052ff935507d5c15aa3c8ba4bab496b298379f1
MarcosAJLC <marcosalexandrejlc@gmail.com>
2025-06-23 14:18:41 -0300

    User Route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,8 +5,6 @@ import LoginReq from "../middlewares/LoginReq.js";
 const router = new Router();
 
 router.post("/", UserC.store);
-router.get("/", UserC.index);
-router.get("/:id", UserC.show);
 router.put("/", LoginReq, UserC.update);
 router.delete("/", LoginReq, UserC.delete);
 export default router;
